ed2791aaf5f3a295e6e3b927734e44dc9e855f34
Wil <wilhelmjanmapogi@gmail.com>
2021-08-11 19:56:34 +0800

    Add prompt to user and computer

--- mainScript.js
+++ mainScript.js
@@ -1,5 +1,6 @@
 function computerPlay() {
     let choices = ["Rock", "Paper", "Scissors"]
+        // returns the random index of choices by maths.
     let choosenHandIndex = Math.floor(Math.random() * choices.length)
     return choices[choosenHandIndex]
 }
