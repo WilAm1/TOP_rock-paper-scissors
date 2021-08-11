8a13fb45eed1328014cb9fb89a9cc706c4be8539
Wil <wilhelmjanmapogi@gmail.com>
2021-08-11 20:03:01 +0800

    Add conditionals on playRound function

--- mainScript.js
+++ mainScript.js
@@ -1,8 +1,22 @@
 function computerPlay() {
-    let choices = ["Rock", "Paper", "Scissors"]
+    let choices = ["rock", "paper", "scissors"]
         // returns the random index of choices by maths.
     let choosenHandIndex = Math.floor(Math.random() * choices.length)
     return choices[choosenHandIndex]
 }
 
-console.log(computerPlay())
+function playRound(player, computer) {
+    let playerWins = `${player} beats ${computer}! player wins!`,
+        computerWins = `${computer} beats ${player}! computer wins!`,
+        draw = `Both are ${player}. It's a draw!`
+    if (player == 'rock' && computer == 'paper') {
+        return
+    }
+}
+
+
+let playerSelection = prompt("Pick Rock, Paper, Scissors!   ").toLowerCase()
+let computerSelection = computerPlay()
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
