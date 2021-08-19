0e10d1aa0cd99c03a003ef039f04de84cf99e9a9
Wil <wilhelmjanmapogi@gmail.com>
2021-08-19 09:53:42 +0800

    Add event listener to buttons

--- mainScript.js
+++ mainScript.js
@@ -1,5 +1,4 @@
 function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
     // returns the random index of choices by maths.
     let choosenHandIndex = Math.floor(Math.random() * choices.length);
     return choices[choosenHandIndex]
@@ -10,54 +9,70 @@ function playRound(player, computer) {
     let playerWins = `${player} > ${computer}! player wins!`,
         computerWins = `${computer} > ${player}! player loses!`,
         draw = `Both are ${player}. It's a draw!`;
-    if (player == computer) {
+    if (player === computer) {
         return draw
-    } else if (player == 'rock' && computer == 'paper') {
+    } else if (player === 'rock' && computer === 'paper') {
         return playerWins
-    } else if (player == 'paper' && computer == 'rock') {
+    } else if (player === 'paper' && computer === 'rock') {
         return playerWins
-    } else if (player == 'scissors' && computer == 'paper') {
+    } else if (player === 'scissors' && computer === 'paper') {
         return playerWins
     } else {
         return computerWins
     }
 }
 
-function game() {
-    let playerScore = 0,
-        computerScore = 0;
 
-    for (let i = 0; i <= 5; ++i) {
+function getPlayerSelection(e) {
+    return e.target.textContent.toLowerCase()
+};
 
-        const playerSelection = prompt("Pick Rock, Paper, Scissors!   ").toLowerCase()
-        const computerSelection = computerPlay()
-        console.log(playerSelection)
-        console.log(computerSelection)
-            // Runs for 1 round and returns the outcome to result variable
-        let result = playRound(playerSelection, computerSelection)
-        console.log(result)
-        if (result.includes('wins')) {
-            playerScore += 1
-        } else if (result.includes('draw')) {
-            computerScore += 1
-            playerScore += 1
-        } else {
-            computerScore += 1
-        }
+// function game() {
+let playerScore = 0,
+    computerScore = 0;
 
-        console.log(`Player Score: ${playerScore}/5\nComputer Score: ${computerScore}/5`);
+const choices = ["rock", "paper", "scissors"];
 
-    }
-    console.clear();
-    console.log('Game over');
-    console.log(`Final Scores!\nPlayer Score: ${playerScore}/5\nComputer Score: ${computerScore}/5`);
+window.addEventListener('click', e => {
+    const playerSelection = e.target.textContent.toLowerCase();
+    if (!choices.includes(playerSelection)) return;
+    const computerSelection = computerPlay()
 
-    if (playerScore == computerScore) {
-        alert("Its a Tie!");
-    } else if (playerScore > computerScore) {
-        alert('Player Wins!');
+    // Runs for 1 round and returns the outcome to result variable
+    let result = playRound(playerSelection, computerSelection)
+    console.log(result)
+    if (result.includes('wins')) {
+        playerScore += 1
+    } else if (result.includes('draw')) {
+        computerScore += 1
+        playerScore += 1
     } else {
-        alert('Player loses. Computer Bot wins!');
+        computerScore += 1
     }
-}
-game();
+
+    console.log(`Player Score: ${playerScore}/5\nComputer Score: ${computerScore}/5`);
+});
+
+
+
+// const weapons = Array.from(document.querySelectorAll('button.weapon'));
+// weapons.forEach(weapon => {
+//     weapon.addEventListener('click', getPlayerSelection);
+// });
+// const playerSelection = null;
+
+
+
+// console.clear();
+// console.log('Game over');
+// console.log(`Final Scores!\nPlayer Score: ${playerScore}/5\nComputer Score: ${computerScore}/5`);
+
+// if (playerScore == computerScore) {
+//     alert("Its a Tie!");
+// } else if (playerScore > computerScore) {
+//     alert('Player Wins!');
+// } else {
+//     alert('Player loses. Computer Bot wins!');
+// }
+// }
+// game();
